5435315e1b3dad438eca4284dc0b7b0da3331ae5
wallstop <dbfsinfil@gmail.com>
2012-12-14 15:22:58 -0500

    Added fix for most memory leaks

--- MPI_Partition.c
+++ MPI_Partition.c
@@ -29,11 +29,14 @@ int localBoard_Size;
 MPI_Comm updatedComm;
 MPI_Group workingProcesses;
 
+void** allocatedMemory;
+int numberOfMemoryAllocations;
+
 //struct partition * partitionArray;
 
 typedef enum
 {
-    NW_UPDATE = 0,
+    NW_UPDATE,
     N_UPDATE,
     NE_UPDATE,
     W_UPDATE,
@@ -45,11 +48,7 @@ typedef enum
     GENERATION_MESSAGE,
     NEIGHBORLIST_MESSAGE,
     BOARD_MESSAGE,
-    PARTITION_MESSAGE,
-    COMM_MESSAGE,
-    ROW_MESSAGE,
-    COLUMN_MESSAGE,
-    COMMUNICATOR_MESSAGE
+    PARTITION_MESSAGE
 } tagType;
 
 bool isAlive(int x, int y); //Prototype
@@ -66,14 +65,22 @@ void setNextArray(int x, int y, int value)
     nextGenBoard[x + (y * (myCoords.lengthX + 2))] = value;
 }
 
+/* Frees all known allocated memory */
+void freeMemory()
+{
+    for(int i = 0; i < numberOfMemoryAllocations; i++)
+        free(allocatedMemory[i]);
+}
+
 /* Parses the input file character by character. Assumes proper file format of ITERATIONS\bCOLUMNS\bROWS\bARRAY_STUFF. Sets the resulting data to *currentGenBoard */
 void parseFile(int argc, char ** argv)
 {
-    //char textMatch[20];
-    //int greatestNumThreads;
+
     int numberOfProcesses;
+
     //Keeps track of the total number of *s and .s in the file
     int counter;
+
     //Keeps track of the current character being read from the file
     char currentChar;
 
@@ -124,44 +131,6 @@ void parseFile(int argc, char ** argv)
     fclose(filePtr);
 }
 
-/* Creates a new comm object including only the threads that are doing work */
-void updateComm()
-{
-    int * groupArray;
-    MPI_Group tempGroup;
-
-    MPI_Comm_group(MPI_COMM_WORLD, &tempGroup);
-    //MPI_Comm_group(MPI_COMM_WORLD, &workingProcesses);
-
-    groupArray = malloc(sizeof(int) * actualPartitions);
-
-    printf("Actual partitions: %d\n", actualPartitions);
-
-    for(int i = 0; i < actualPartitions; i++)
-    {
-        //MPI_Group_incl(workingProcesses, 1, &i, &workingProcesses);
-        groupArray[i] = i;
-    }
-
-    printf("Attempting to include in group...\n");
-    MPI_Group_incl(tempGroup, actualPartitions, groupArray, &workingProcesses);
-
-    printf("Included in group!");
-    free(groupArray);
-
-    MPI_Comm_create(MPI_COMM_WORLD, workingProcesses, &updatedComm);
-
-    printf("Comm created!\n");
-
-    //for(int i = 0; i < actualPartitions; i++)
-    //{
-    //    MPI_Isend(&actualPartitions, 1, MPI_INT, i, PARTITION_MESSAGE, MPI_COMM_WORLD, &lastRequest);
-    //    MPI_Isend(&updatedComm, actualPartitions, MPI_INT, i, COMM_MESSAGE, MPI_COMM_WORLD, &lastRequest);
-    //}
-
-    //printf("Messages sent!\n");
-}
-
 /* Called when the final board configurations have been calculated, all processes submit their sections for gather */
 void finalizeBoard()
 {
@@ -170,6 +139,7 @@ void finalizeBoard()
     if(!identity)
     {
         char* incomingBoard;
+
         for(int i = 0; i < actualPartitions; i++)
         {
             size = (partitionArray[i].lengthX + 2) * (partitionArray[i].lengthY + 2);
@@ -194,17 +164,11 @@ void finalizeBoard()
                 {
                     int clientEquivLocation = ( (k+1) * (partitionArray[i].lengthX+2) + (j+1) );
 
-                    //printf("Looking at client index: %d to master index: %d\n", clientEquivLocation, j + k * masterBoard_columns);
                     masterBoard[ j + partitionArray[i].startX + ( ( k+partitionArray[i].startY ) * masterBoard_columns )] = incomingBoard[clientEquivLocation];
-                    //Convert startx to original array position
-                    //
-                    //walkthough each position in the client array, making sure to take into conderation bounds
                 }
             }
+
             free(incomingBoard);
-
-
-            //print board?
         }
 
         printf("Final board configuration: \n");
@@ -225,6 +189,8 @@ void finalizeBoard()
         MPI_Barrier(MPI_COMM_WORLD);   //UPDATED COMM
     }
 
+    freeMemory();   //Frees all malloc'd memory
+
     MPI_Finalize();
 }
 
@@ -238,6 +204,9 @@ void initializeBoard(int argc, char ** argv)
     parseFile(argc, argv);
 
     partitionArray = generateBoard(masterBoard_columns, masterBoard_rows, &actualPartitions);
+
+    numberOfMemoryAllocations = actualPartitions;
+    allocatedMemory = malloc(sizeof(char*) * (numberOfMemoryAllocations));
 
     for(int i = 0; i < actualPartitions; i++)
     {
@@ -261,9 +230,10 @@ void initializeBoard(int argc, char ** argv)
                     curLoopBoard[l++] = masterBoard[j + (k * masterBoard_columns)];
             }
         }
-        //MPI_Isend part of the board they want
+
+        allocatedMemory[i] = (void*)curLoopBoard;
+
         MPI_Isend(curLoopBoard, curLoopBoard_Size, MPI_CHAR, i, BOARD_MESSAGE, MPI_COMM_WORLD, &lastRequest);
-        //free(curLoopBoard);
     }
 
     printf("Initial board: \n\n");
@@ -278,10 +248,7 @@ void initializeBoard(int argc, char ** argv)
             printf("\n");
     }
     for(int i = 0; i < actualPartitions; i++)
-    {
         MPI_Isend(&actualPartitions, 1, MPI_INT, i, PARTITION_MESSAGE, MPI_COMM_WORLD, &lastRequest);
-
-    }
 }
 
 /* Updates the board */
@@ -301,65 +268,68 @@ void calculateBoard()
         {
             for(int j = 0; j < 8; j++)
             {
-                switch(j)
-                {
-                case 0:
-                    sendEdge = malloc(sizeof(char) * 1);
-                    sendEdge[0] = localBoard[myCoords.lengthX + 3];
-                    size = 1;
-                    tag = SE_UPDATE;
-                    break;
-                case 1:
-                    sendEdge = malloc(sizeof(char) * myCoords.lengthX);
-                    for(int k = 0; k < myCoords.lengthX; k++)
-                        sendEdge[k] = localBoard[myCoords.lengthX + 3 + k];
-                    size = myCoords.lengthX;
-                    tag = S_UPDATE;
-                    break;
-                case 2:
-                    sendEdge = malloc(sizeof(char) * 1);
-                    sendEdge[0] = localBoard[myCoords.lengthX + 2 + myCoords.lengthX];
-                    size = 1;
-                    tag = SW_UPDATE;
-                    break;
-                case 3:
-                    sendEdge = malloc(sizeof(char) * myCoords.lengthY);
-                    for(int k = 0; k < myCoords.lengthY; k++)
-                        sendEdge[k] = localBoard[(myCoords.lengthX + 3) + k * (myCoords.lengthX + 2)];
-                    size = myCoords.lengthY;
-                    tag = E_UPDATE;
-                    break;
-                case 4:
-                    sendEdge = malloc(sizeof(char) * myCoords.lengthY);
-                    for(int k = 0; k < myCoords.lengthY; k++)
-                        sendEdge[k] = localBoard[(myCoords.lengthX + 2 + myCoords.lengthX) + k * (myCoords.lengthX + 2)];
-                    size = myCoords.lengthY;
-                    tag = W_UPDATE;
-                    break;
-                case 5:
-                    sendEdge = malloc(sizeof(char) * 1);
-                    sendEdge[0] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + 1];
-                    size = 1;
-                    tag = NE_UPDATE;
-                    break;
-                case 6:
-                    sendEdge = malloc(sizeof(char) * myCoords.lengthX);
-                    for(int k = 0; k < myCoords.lengthX; k++)
-                        sendEdge[k] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + 1 + k];
-                    size = myCoords.lengthX;
-                    tag = N_UPDATE;
-                    break;
-                case 7:
-                    sendEdge = malloc(sizeof(char) * 1);
-                    sendEdge[0] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + myCoords.lengthX];
-                    size = 1;
-                    tag = NW_UPDATE;
-                    break;
-                }
-
                 if(myNeighborIDs[j] > -1)
+                {
+                    switch(j)
+                    {
+                    case 0:
+                        sendEdge = malloc(sizeof(char) * 1);
+                        sendEdge[0] = localBoard[myCoords.lengthX + 3];
+                        size = 1;
+                        tag = SE_UPDATE;
+                        break;
+                    case 1:
+                        sendEdge = malloc(sizeof(char) * myCoords.lengthX);
+                        for(int k = 0; k < myCoords.lengthX; k++)
+                            sendEdge[k] = localBoard[myCoords.lengthX + 3 + k];
+                        size = myCoords.lengthX;
+                        tag = S_UPDATE;
+                        break;
+                    case 2:
+                        sendEdge = malloc(sizeof(char) * 1);
+                        sendEdge[0] = localBoard[myCoords.lengthX + 2 + myCoords.lengthX];
+                        size = 1;
+                        tag = SW_UPDATE;
+                        break;
+                    case 3:
+                        sendEdge = malloc(sizeof(char) * myCoords.lengthY);
+                        for(int k = 0; k < myCoords.lengthY; k++)
+                            sendEdge[k] = localBoard[(myCoords.lengthX + 3) + k * (myCoords.lengthX + 2)];
+                        size = myCoords.lengthY;
+                        tag = E_UPDATE;
+                        break;
+                    case 4:
+                        sendEdge = malloc(sizeof(char) * myCoords.lengthY);
+                        for(int k = 0; k < myCoords.lengthY; k++)
+                            sendEdge[k] = localBoard[(myCoords.lengthX + 2 + myCoords.lengthX) + k * (myCoords.lengthX + 2)];
+                        size = myCoords.lengthY;
+                        tag = W_UPDATE;
+                        break;
+                    case 5:
+                        sendEdge = malloc(sizeof(char) * 1);
+                        sendEdge[0] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + 1];
+                        size = 1;
+                        tag = NE_UPDATE;
+                        break;
+                    case 6:
+                        sendEdge = malloc(sizeof(char) * myCoords.lengthX);
+                        for(int k = 0; k < myCoords.lengthX; k++)
+                            sendEdge[k] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + 1 + k];
+                        size = myCoords.lengthX;
+                        tag = N_UPDATE;
+                        break;
+                    case 7:
+                        sendEdge = malloc(sizeof(char) * 1);
+                        sendEdge[0] = localBoard[(myCoords.lengthX + 2) * myCoords.lengthY + myCoords.lengthX];
+                        size = 1;
+                        tag = NW_UPDATE;
+                        break;
+                    }
+
+                    allocatedMemory[numberOfMemoryAllocations++] = (void*)sendEdge;
+
                     MPI_Isend(sendEdge, size, MPI_CHAR, myNeighborIDs[j], tag, MPI_COMM_WORLD, &lastRequest);
-                //free(sendEdge);
+                }
             }
 
             /* Receive */
@@ -449,7 +419,7 @@ void calculateBoard()
                         break;
                     }
 
-                    //free(recvEdge);
+                    free(recvEdge);
                 }
             }
 
@@ -465,19 +435,9 @@ void calculateBoard()
                 }
             }
 
-            printf("\nLocal Board Process:%d Generation:%d\n",identity,numberOfGenerations);
-            for(int i = 0; i < (myCoords.lengthY+2) * (myCoords.lengthX+2); i++)
-            {
-                if(localBoard[i] == 1)
-                    printf("*");
-                else
-                    printf(".");
-                if((i + 1) % (myCoords.lengthX+2) == 0)
-                    printf("\n");
-            }
-
             swapBoards();
         }
+
         MPI_Barrier(MPI_COMM_WORLD);   //Once done, chill out till everyon else is done.   //UPDATED COMM
     }
 
@@ -518,7 +478,7 @@ bool isAlive(int x, int y)
 //Swaps the current gen board with the next gen board to save from mallocing tons of boards
 void swapBoards(void)
 {
-    char * tempBoard;
+    char* tempBoard;
 
     tempBoard = localBoard;
     localBoard = nextGenBoard;
@@ -529,6 +489,8 @@ void initMPI(int argc, char ** argv)
 {
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &identity);
+
+    numberOfMemoryAllocations = 0;
 
     if(!identity)
     {
